3dc506806fe8b46db82cc0a9a9d1ded8df2edc09
PDXing <1143913447@qq.com>
2021-06-29 22:52:37 +0800

    添加了jira-tool

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.css';
 
-import { ProjectListScreen } from './screens/project-list';
+// import { ProjectListScreen } from './screens/project-list';
 import { LoginScreen } from './screens/lgoin';
 
 function App() {
